186a7897758e57c277bfe0910aff814b551921f6
Yahya Wessam <yahyawessam2002@gmail.com>
2016-04-21 19:41:53 +0300

    Add V4A

--- turbo.mk
+++ turbo.mk
@@ -6,6 +6,10 @@ $(call inherit-product, vendor/turbo/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/turbo/config/common_full_phone.mk)
 
+# ViPER4Android
+PRODUCT_PACKAGES += \
+    ViPER4Android
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
   PRODUCT_NAME=jfltetmo \
   TARGET_DEVICE=jfltetmo \
